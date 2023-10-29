9e2e99f66a3509aec61439505e2d6b6a1f99748a
Max Xue <maxxue1512@gmail.com>
2023-10-29 18:13:58 +1100

    Updated keymaps for git

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -32,20 +32,24 @@ vim.keymap.set("n", "<leader>fp", ":Telescope projects<CR>", { desc = "Find proj
 -- Git
 local gitsigns = require("gitsigns")
 vim.keymap.set("n", "<leader>hh", ":LazyGit<cr>", { desc = "Open LazyGit" })
--- vim.keymap.set("n", "<leader>hd", gitsigns.toggle_deleted, { desc = "Toggle git deleted" })
 vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff file" })
+vim.keymap.set("n", "<leader>hD", ":Gitsigns diffthis ", { desc = "Diff file (commit/branch)" })
+vim.keymap.set("n", "<leader>ht", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
 vim.keymap.set("n", "<leader>hb", gitsigns.blame_line, { desc = "Git blame line" })
 
-vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Git preview hunk" })
+vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+vim.keymap.set("n", "<leader>hP", gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
 vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
 vim.keymap.set("n", "<leader>hu", gitsigns.reset_buffer_index, { desc = "Unstage all hunks in file" })
 
-vim.keymap.set("n", "<leader>hk", gitsigns.next_hunk, { desc = "Git next hunk" })
-vim.keymap.set("n", "<leader>hj", gitsigns.prev_hunk, { desc = "Git prev hunk" })
+vim.keymap.set("n", "<leader>hk", gitsigns.next_hunk, { desc = "Next hunk" })
+vim.keymap.set("n", "<leader>hj", gitsigns.prev_hunk, { desc = "Prev hunk" })
 
 vim.keymap.set("n", "<leader>hrb", gitsigns.reset_buffer, { desc = "Git reset buffer" })
 vim.keymap.set("n", "<leader>hrh", gitsigns.reset_hunk, { desc = "Git reset hunk" })
 
+-- https://www.reddit.com/r/neovim/comments/vlc9sc/how_to_define_a_user_command_to_partially_stage/?sort=new
+-- Comment by andrewfz
 local function visual_stage()
 	local first_line = vim.fn.line("v")
 	local last_line = vim.fn.getpos(".")[2]
