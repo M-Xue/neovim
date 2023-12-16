246707a0a2f74883cbd35d057001eb8c1758f627
Max Xue <maxxue1512@gmail.com>
2023-12-17 10:43:09 +1100

    Remove status line from zen mode

--- lua/plugins/misc/ui/zen-mode.lua
+++ lua/plugins/misc/ui/zen-mode.lua
@@ -1,38 +1,47 @@
-local nvim_tree_config = require("plugins.core.nvim-tree")
-
 require("zen-mode").setup({
 	options = {
 		signcolumn = "yes", -- disable signcolumn
 		number = true,
 		relativenumber = true,
-		list = true,
 	},
 	plugins = {
 		options = {
-			laststatus = 3,
+			-- laststatus = 3,
 		},
 		gitsigns = { enabled = false },
 	},
-	-- callback where you can add custom code when the Zen window opens
-	-- on_open = function()
-	-- 	require("nvim-tree").setup(vim.tbl_deep_extend("force", {
-	-- 		view = {
-	-- 			width = 35,
-	-- 			float = {
-	-- 				enable = true,
-	-- 			},
-	-- 		},
-	-- 	}, nvim_tree_config))
-	-- end,
-	-- -- callback where you can add custom code when the Zen window closes
-	-- on_close = function()
-	-- 	require("nvim-tree").setup(vim.tbl_deep_extend("force", {
-	-- 		view = {
-	-- 			width = 35,
-	-- 			float = {
-	-- 				enable = false,
-	-- 			},
-	-- 		},
-	-- 	}, nvim_tree_config))
-	-- end,
 })
+
+-- local nvim_tree_config = require("plugins.core.nvim-tree")
+-- callback where you can add custom code when the Zen window opens
+-- on_open = function()
+-- 	require("nvim-tree").setup(vim.tbl_deep_extend("force", {
+-- 		view = {
+-- 			width = 35,
+-- 			float = {
+-- 				enable = true,
+-- 			},
+-- 		},
+-- 	}, nvim_tree_config))
+-- end,
+-- -- callback where you can add custom code when the Zen window closes
+-- on_close = function()
+-- 	require("nvim-tree").setup(vim.tbl_deep_extend("force", {
+-- 		view = {
+-- 			width = 35,
+-- 			float = {
+-- 				enable = false,
+-- 			},
+-- 		},
+-- 	}, nvim_tree_config))
+-- end,
+--
+--
+-- asdf
+-- asdfasdfasd
+-- asdf
+--
+--
+--
+--
+--
