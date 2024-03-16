fd19acf47c701a6a07f9c6f53f5464a33e1c0623
Max Xue <maxxue1512@gmail.com>
2024-03-17 00:16:48 +1100

    more colourschemes

---
+++ lua/plugins/colourscheme/catppuccin.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato", -- latte, frappe, macchiato, mocha
+				show_end_of_buffer = true,
+				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+					comments = { "italic" }, -- Change the style of comments
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					alpha = true,
+					mason = true,
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+}
---
+++ lua/plugins/colourscheme/init.lua
@@ -0,0 +1,7 @@
+return {
+	require("plugins.colourscheme.catppuccin"),
+	-- require("plugins.colourscheme.kanagawa"),
+	-- require("plugins.colourscheme.gruvbox"),
+	-- require("plugins.colourscheme.tokyo-night"),
+	-- require("plugins.colourscheme.everforest"),
+}
