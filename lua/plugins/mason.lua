3ab6b4dff0424cebe469b1343b9adb683f23dfc5
Max Xue <maxxue1512@gmail.com>
2025-08-31 16:51:22 +1000

    fix mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -14,35 +14,35 @@ return {
 			max_concurrent_installers = 4,
 		},
 	},
-	-- {
-	-- 	"mason-org/mason-lspconfig.nvim",
-	-- 	dependencies = {
-	-- 		"mason-org/mason.nvim",
-	-- 		"neovim/nvim-lspconfig",
-	-- 	},
-	-- 	config = function()
-	-- 		require("mason-lspconfig").setup({
-	-- 			automatic_enable = {
-	-- 				"marksman",
-	-- 				"mdx_analyzer",
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				automatic_enable = {
+					"marksman",
+					"mdx_analyzer",
 
-	-- 				"html",
-	-- 				"emmet_language_server",
-	-- 				"cssls",
-	-- 				"cssmodules_ls",
-	-- 				"tailwindcss",
-	-- 				"jsonls",
-	-- 				"astro",
-	-- 				"svelte",
-	-- 				"ts_ls",
+					"html",
+					"emmet_language_server",
+					"cssls",
+					"cssmodules_ls",
+					"tailwindcss",
+					"jsonls",
+					"astro",
+					"svelte",
+					"ts_ls",
 
-	-- 				"gopls",
-	-- 				"lua_ls",
-	-- 				"vimls",
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
+					"gopls",
+					"lua_ls",
+					"vimls",
+				},
+			})
+		end,
+	},
 	{
 		"zapling/mason-conform.nvim",
 		dependencies = {
@@ -62,21 +62,21 @@ return {
 			automatic_installation = true,
 		},
 	},
-	-- {
-	-- 	"rshkarin/mason-nvim-lint",
-	-- 	dependencies = {
-	-- 		"williamboman/mason.nvim",
-	-- 		"mfussenegger/nvim-lint",
-	-- 	},
-	-- 	opts = {
-	-- 		ensure_installed = {
-	-- 			"eslint_d",
-	-- 			"stylelint",
-	-- 			"golangci-lint",
-	-- 			"markdownlint-cli2",
-	-- 			"cspell",
-	-- 		},
-	-- 		automatic_installation = true,
-	-- 	},
-	-- },
+	{
+		"rshkarin/mason-nvim-lint",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-lint",
+		},
+		opts = {
+			ensure_installed = {
+				"eslint_d",
+				"stylelint",
+				-- "golangci-lint", -- These two are bugged for some reason
+				-- "markdownlint-cli2",
+				"cspell",
+			},
+			automatic_installation = true,
+		},
+	},
 }
