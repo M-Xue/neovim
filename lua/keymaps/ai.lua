bd4fa8c545dec601547295325a0f34dd88b225c3
Max Xue <maxxue1512@gmail.com>
2025-11-20 21:23:51 +1100

    new chat avante binding

--- lua/keymaps/ai.lua
+++ lua/keymaps/ai.lua
@@ -7,5 +7,6 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader>app", ":Copilot panel toggle<cr>", { desc = "Toggle Copilot suggestion panel" })
 vim.keymap.set("n", "<leader>apr", ":Copilot panel refresh<cr>", { desc = "Refresh Copilot suggestion panel" })
 vim.keymap.set("n", "<leader>aa", ":AvanteAsk<cr>", { desc = "Avante: Toggle chat" })
-vim.keymap.set("n", "<leader>am", ":AvanteModels<cr>", { desc = "Avante: See models" })
+vim.keymap.set("n", "<leader>an", ":AvanteChatNew<cr>", { desc = "Avante: New chat" })
+vim.keymap.set("n", "<leader>am", ":AvanteModels<cr>", { desc = "Avante: See Togglemodels" })
 vim.keymap.set("v", "<leader>aa", ":AvanteEdit<cr>", { desc = "Avante: Edit selected blocks" })
