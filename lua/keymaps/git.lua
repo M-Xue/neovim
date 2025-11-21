bb97aaaa1169027d7c7c334a03bd57afff9704a7
Max Xue <maxxue1512@gmail.com>
2025-11-22 01:25:26 +1100

    Clean up git keymaps

--- lua/keymaps/git.lua
+++ lua/keymaps/git.lua
@@ -1,20 +1,5 @@
 local gitsigns = require("gitsigns")
 
--- Stage buffer
--- Toggle deleted
--- Undo stage hunk
---
--- Current branch changes - this is what we are looking for in PR reviews but try find a better UX for it
---
--- change preview hunk to be inline just to see how we feel about it
-
--- try get these in diffview
--- git history for line
--- git history for file
--- git history for all commits in diffview? this will allow me to check commit diffs
--- git histroy between current branch particular commit
--- even better if its against where it branched off master
-
 vim.keymap.set("n", "<leader>hh", ":LazyGit<cr>", { desc = "Open LazyGit" })
 
 -- Hunks
@@ -23,8 +8,7 @@ vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
 vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buffer" })
 vim.keymap.set("n", "<leader>hj", gitsigns.next_hunk, { desc = "Next hunk" })
 vim.keymap.set("n", "<leader>hk", gitsigns.prev_hunk, { desc = "Prev hunk" })
-vim.keymap.set("n", "<leader>hP", gitsigns.preview_hunk, { desc = "Preview hunk in float" })
-vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
+vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk_inline, { desc = "Preview hunk" })
 
 -- Permalink
 vim.keymap.set({ "n", "v" }, "<leader>hl", ":.GBrowse master:%", { desc = "Open permalink in remote" })
@@ -38,32 +22,22 @@ local function toggle_diffview()
 	end
 end
 vim.keymap.set("n", "<leader>hd", toggle_diffview, { desc = "Diff index" })
-vim.keymap.set("n", "<leader>hm", ":DiffviewOpen origin/master...HEAD", { desc = "Diff merge base" })
+vim.keymap.set("n", "<leader>hm", ":DiffviewOpen origin/master...HEAD<cr>", { desc = "Diff merge base" })
 
 -- Blame
-vim.keymap.set("n", "<leader>hb", gitsigns.blame_line, { desc = "Git blame line" })
+vim.keymap.set("n", "<leader>hb", gitsigns.blame_line, { desc = "Blame line" })
+
+-- Git history
 vim.keymap.set(
 	"n",
-	"<leader>hB",
+	"<leader>hl",
 	":Telescope advanced_git_search diff_commit_line<cr>",
 	{ desc = "Git commit history for line" }
 )
 vim.keymap.set(
 	"n",
-	"<leader>h<c-b>",
+	"<leader>hf",
 	":Telescope advanced_git_search diff_commit_file<cr>",
 	{ desc = "Git commit history for file" }
 )
-vim.keymap.set(
-	"n",
-	"<leader>h<c-B>",
-	":Telescope advanced_git_search diff_branch_file<cr>",
-	{ desc = "Diff against branch" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>h<c-B>",
-	":Telescope advanced_git_search changed_on_branch<cr>",
-	{ desc = "Current branch changes" }
-)
-vim.keymap.set("n", "<leader>hH", ":DiffviewFileHistory<cr>", { desc = "Repo history" })
+vim.keymap.set("n", "<leader>ho", ":DiffviewFileHistory<cr>", { desc = "Git log" })
